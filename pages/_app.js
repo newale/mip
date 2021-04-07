ddcc9503546f4e65791ba2496ac4bab6ced0d31e
ale <alejandrob@live.cl>
2021-04-07 00:01:26 -0400

    chore: Fixed linting

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import App from 'next/app';
 import { Provider as StyletronProvider } from 'styletron-react';
-import {BaseProvider, LightTheme} from 'baseui';
+import { BaseProvider, LightTheme } from 'baseui';
 import 'react-flow-renderer/dist/style.css';
 import '../src/styles/index.css';
 import { styletron } from '../styletron';
